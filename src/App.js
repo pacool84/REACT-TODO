2d487d48f5ea05a65e53001f4c1d05052bd7d68b
Francisco López <flopez_1384@hotmail.com>
2021-11-06 16:27:32 -0600

    [ADD] The basic template of the application in the App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import "./App.css";
 
+const todos = [
+  { text: "Enviar ordenes de compra", complete: false },
+  { text: "Comprar comida", complete: false },
+  { text: "Comprar bebida", complete: false },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/*  <TodoCounter /> */}
+      <h2>Has completado 2 de 3 TODO´s</h2>
+      {/* <TodoSearch /> */}
+      <input placeholder="Search a ToDo" />
+      {/*  <TodoList>
+        {todos.map((todo) => (
+          <TodoItem />
+        ))}
+      </TodoList> */}
+      {/* <CreateTodoButton /> */}
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
