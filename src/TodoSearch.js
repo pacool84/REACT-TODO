4cb1e8efc7903389c498fa627aaab2781d1d27d8
Francisco López <flopez_1384@hotmail.com>
2021-11-09 22:06:53 -0600

    [ADD] A state in to the TodoSearch component

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = useState("");
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
   return (
-    <input
-      className="TodoSearch"
-      placeholder="Search a ToDo"
-      onChange={onSearchValueChange}
-    />
+    <Fragment>
+      <input
+        className="TodoSearch"
+        placeholder="Search a ToDo"
+        onChange={onSearchValueChange}
+        value={searchValue}
+      />
+      <p>{searchValue}</p>
+    </Fragment>
   );
 }
 
