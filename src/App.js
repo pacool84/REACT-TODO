855dccb76df870b457f4cfb1afbf02ac7d14ce9e
Francisco López <flopez_1384@hotmail.com>
2021-11-16 21:34:13 -0600

    [ADD] The logig to the search bar for find todo

--- src/App.js
+++ src/App.js
@@ -16,9 +16,23 @@ const defaultTodos = [
 function App() {
   const [searchValue, setSearchValue] = useState("");
   const [todos, setTodos] = useState(defaultTodos);
+
   const totalTodos = todos.length;
   const completedTodos = todos.filter((todo) => todo.completed === true).length;
   const pendingTodos = todos.filter((todo) => todo.completed === false).length;
+
+  /* Logic to the search Bar */
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
   return (
     <React.Fragment>
       <TodoCounter
@@ -30,7 +44,7 @@ function App() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
