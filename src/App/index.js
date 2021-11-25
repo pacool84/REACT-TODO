750af11a70e5d82733b41f767834d2f3049a7762
Francisco López <flopez_1384@hotmail.com>
2021-11-24 21:40:43 -0600

    [ADD] Part of a cutom HOOK to use the local storage

--- src/App/index.js
+++ src/App/index.js
@@ -9,19 +9,31 @@ import { AppUI } from "./AppUI";
   { text: "Comprar ropa", completed: false },
 ]; */
 
-function App() {
-  const localStorageTodos = localStorage.getItem("TODOS_V1");
-  let parsedTodos;
+/* Custom Hook */
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
 
   //Verificamos si hay contenido en el localStorage
-  if (!localStorageTodos) {
-    localStorage.setItem("TODOS_V1", JSON.stringify([]));
-    parsedTodos = [];
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   } else {
-    parsedTodos = JSON.parse(localStorageTodos);
+    parsedItem = JSON.parse(localStorageItem);
   }
+  const [item, setItem] = useState(parsedItem);
 
-  const [todos, setTodos] = useState(parsedTodos);
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+  return [item, saveItem];
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
+
   const [searchValue, setSearchValue] = useState("");
 
   const completedTodos = todos.filter((todo) => todo.completed === true).length;
@@ -29,7 +41,7 @@ function App() {
   const pendingTodos = todos.filter((todo) => todo.completed === false).length;
 
   /* Logic to the search Bar */
-  let searchedTodos = [];
+  let searchedTodos = initialValue;
 
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
@@ -40,12 +52,6 @@ function App() {
       return todoText.includes(searchText);
     });
   }
-
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem("TODOS_V1", stringifiedTodos);
-    setTodos(newTodos);
-  };
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
