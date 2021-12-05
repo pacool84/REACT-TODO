3cc48d5758c69a095b07fea33c7337009b215063
Francisco López <flopez_1384@hotmail.com>
2021-12-05 16:41:08 -0600

    [ADD] useContext to improve the way to pass props

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,35 +7,27 @@ import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
 function AppUI() {
+  const { error, loading, searchedTodos, completeTodo, deleteTodo } =
+    React.useContext(TodoContext);
   return (
     <React.Fragment>
       <TodoCounter />
       <TodoSearch />
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
 
-      <TodoContext.Consumer>
-        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => {
-          return (
-            <TodoList>
-              {error && <p>Desespérate, hubo un error...</p>}
-              {loading && <p>Estamos cargando, no desesperes...</p>}
-              {!loading && !searchedTodos.length && (
-                <p>¡Crea tu primer TODO!</p>
-              )}
-
-              {searchedTodos.map((todo) => (
-                <TodoItem
-                  key={todo.text}
-                  text={todo.text}
-                  completed={todo.completed}
-                  onComplete={() => completeTodo(todo.text)}
-                  onDelete={() => deleteTodo(todo.text)}
-                />
-              ))}
-            </TodoList>
-          );
-        }}
-      </TodoContext.Consumer>
-
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
       <CreateTodoButton />
     </React.Fragment>
   );
